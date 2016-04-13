96ab21c49ef1303e76dd93fbb0a5d8ded3ffc0f1
Alejandro Quadri <alejandroquadri@gmail.com>
2016-04-12 21:31:32 -0300

    Terminado!

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -101,21 +101,40 @@ app.controller('MainCtrl',function(posteos, auth){
 })
 
 .controller('AuthCtrl', function($scope, $state, auth){
+
   $scope.user = {};
 
-  $scope.register = function(){
-    auth.register($scope.user).error(function(error){
-      $scope.error = error;
-    }).then(function(){
-      $state.go('home');
-    });
-  };
+  // $scope.register = function(){
+  //   auth.register($scope.user).error(function(error){
+  //     $scope.error = error;
+  //   }).then(function(){
+  //     $state.go('home');
+  //   });
+  // };
+
+//   $scope.logIn = function(){
+//     auth.logIn($scope.user).error(function(error){
+//       $scope.error = error;
+//     }).then(function(){
+//       $state.go('home');
+//     });
+//   };
+// })
+
+$scope.register = function(){
+  auth.register($scope.user).then(function(){
+    $state.go('home');
+  }, function(error){
+    $scope.error = error.data;
+  });
+};
 
   $scope.logIn = function(){
-    auth.logIn($scope.user).error(function(error){
-      $scope.error = error;
-    }).then(function(){
+    auth.logIn($scope.user).then(function(){
       $state.go('home');
+    }, function(error){
+      console.log(error);
+      $scope.error = error.data;
     });
   };
 })
